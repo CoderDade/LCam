5c80f6ff92f6b97650213877172b02ba0f8f8c20
CoderDade <coder.dade@gmail.com>
2018-03-16 20:43:08 +0800

    build prj and add ignore files

---
+++ daily-mail/src/main/java/com/dade/lcam/dailymail/DailyMailApplication.java
@@ -0,0 +1,12 @@
+package com.dade.lcam.dailymail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMailApplication.class, args);
+	}
+}
